543d1c9adfc425641c158a29388a288e5d06a5e5
Wolverine1621 <Wolverine1621@users.noreply.github.com>
2017-12-20 20:52:53 -0500

    Button tweaks, removed timetype

--- js/jquery.js
+++ js/jquery.js
@@ -1,27 +1,29 @@
 var main = function() {
+    $("#hidingbuttons").toggle(); // Hide buttons at beginning
+
     $("#main").click(function() { // Get a different animation
         $("#hexcolor").fadeToggle();
     });
-            
+
     $("#hidebtn").click(function() {
         $("#hidingbuttons").fadeToggle();
-        
+
         var $this = $(this);
         $this.toggleClass('h');
             if($this.hasClass('h')){
-        $this.text('Show');         
+        $this.text('Hide');
         } else {
-            $this.text('Hide');
+            $this.text('Info');
         }
     });
-            
+
     $("#utcbtn").click(function() {
         $("#timetype").fadeToggle();
-        
+
         var $this = $(this);
         $this.toggleClass('utc');
             if($this.hasClass('utc')){
-        $this.text('Local');         
+        $this.text('Local');
         } else {
             $this.text('UTC');
         }
